ec770b6f6bc8a03f78318deabe1a5bfbdd1ce818
zenbenten <joshuaelsner97@gmail.com>
2025-06-22 21:22:02 -0400

    grapple anchor, player anchor, and visible rope. pendulum swings as expected

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -2,26 +2,26 @@ extends CharacterBody2D
 
 @export var speed = 10.0
 @export var jump_power = 10.0
-
 var speed_multiplier = 30.0
 var jump_multiplier = -30.0
 var direction = 0
 
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
-
+var has_grappling_hook: bool = false
+var is_grappling: bool = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	if not is_grappling:
+		handle_movement(delta)
+
+	handle_grappling(delta)
+
+func handle_movement(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_power * jump_multiplier
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * speed * speed_multiplier
@@ -29,3 +29,23 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
 
 	move_and_slide()
+
+func handle_grappling(delta: float) -> void:
+	if not has_grappling_hook:
+		return
+
+	if Input.is_action_just_pressed("ui_home"):
+		for g in get_tree().get_nodes_in_group("grappling-hook-system"):
+			var success = g.attach_player(self)
+			if success == 0:
+				is_grappling = true
+				break
+	elif Input.is_action_just_released("ui_home"):
+		for g in get_tree().get_nodes_in_group("grappling-hook-system"):
+			var success = g.detach_player(self)
+			if success == 0:
+				is_grappling = false
+				break
+
+	# Prevent rotation when attached
+	global_rotation = 0
